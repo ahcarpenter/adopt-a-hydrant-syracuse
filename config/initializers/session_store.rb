f54b613d01adb2a4695623312a71e1dda9c6bdad
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-29 18:48:17 -0400

    update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ AdoptAThing::Application.config.session_store :cookie_store, :key => '_adopt-a-t
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# AdoptAThing::Application.config.session_store :active_record_store
+AdoptAThing::Application.config.session_store :active_record_store
