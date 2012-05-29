f3b7b40310ede144e86f856290ee744584f7abba
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-05-29 17:56:23 -0400

    update

--- app/controllers/info_window_controller.rb
+++ app/controllers/info_window_controller.rb
@@ -1,6 +1,7 @@
 # http://oldwiki.rubyonrails.org/rails/pages/HowtoWorkWithSessions
 class InfoWindowController < ApplicationController
   def index
+    session[:thing] = nil
     session[:thing] = Thing.find_by_id(params[:thing_id])
     session[:thing] = Thing.find_by_id(session[:id]) if session[:thing].nil?
     
