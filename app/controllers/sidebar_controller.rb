3bffe69ef18ab9d2739a14492059bce309dcefa9
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-06-24 19:24:14 -0400

    update

--- app/controllers/sidebar_controller.rb
+++ app/controllers/sidebar_controller.rb
@@ -1,6 +1,10 @@
 class SidebarController < ApplicationController
+  begin
   def combo_form
     @required = Sidebar.translate_required if @required.nil?
     @sms_notifications = Sidebar.translate_sms_notifications if @sms_notifications.nil?
+  end
+  rescue ActionView::Template::Error
+    puts 'Hello'
   end
 end
