dc91ac2391250d3bd120cef1440837b11210813b
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-06-24 21:59:35 -0400

    revert

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,8 +3,7 @@ class MainController < ApplicationController
 
   def index
     @name = User.find(User.current.id).name if !User.current.nil?
-    @portion_of_preview = ' has referred you to Adopt-a-Hydrant. You might be interested in checking out the following: http://adopt-a-hydrant-syracuse.herokuapp.com/' + Base64::encode64(((Referral.all.last.id)+1).to_s).chop + '.'
-    
+        
     begin
     Referral.generate_referral(params[:referral]) if !params[:referral].nil?
     rescue Net::SMTPSyntaxError
