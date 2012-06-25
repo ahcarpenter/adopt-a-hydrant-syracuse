ecfd068e4daa86fa4837c6346617c5a3261bde63
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-06-24 20:00:03 -0400

    update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,11 +2,14 @@ class MainController < ApplicationController
   before_filter :set_current_user
 
   def index
+    @name = User.find(User.current.id).name
+    
     begin
     Referral.generate_referral(params[:referral]) if !params[:referral].nil?
     rescue Net::SMTPSyntaxError
       puts 'Hello'
     end
+    
     @required = Sidebar.translate_required if @required.nil?
     @sms_notifications = Sidebar.translate_sms_notifications if @sms_notifications.nil?
   end
