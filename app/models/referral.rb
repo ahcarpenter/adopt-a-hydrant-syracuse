bc182905760fdceeabf70309b47fe5caa084c83e
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-06-28 01:10:20 -0400

    refactor

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -11,20 +11,22 @@ class Referral < ActiveRecord::Base
   
   def self.generate_referral(attributes)
       if attributes[:phone_number] != ''
-        if Referee.exists?(:endpoint => SMS.sieve(attributes[:phone_number]))
-          referee = Referee.find_by_endpoint(SMS.sieve(attributes[:phone_number]).to_s)
+        phone_number = SMS.sieve(attributes[:phone_number])
+        if Referee.exists?(:endpoint => phone_number)
+          referee = Referee.find_by_endpoint(phone_number.to_s)
         else
-          referee = Referee.new.set_endpoint(SMS.sieve(attributes[:phone_number]).to_s)
+          referee = Referee.new.set_endpoint(phone_number.to_s)
         end
         referral = Referral.new.construct(referee.id)
         SMS.send_referral(referral)
       end
       
       if attributes[:email_address] != ''
-        if Referee.exists?(:endpoint => attributes[:email_address])
-          referee = Referee.find_by_endpoint(attributes[:email_address])
+        email_address = attributes[:email_address]
+        if Referee.exists?(:endpoint => email_address)
+          referee = Referee.find_by_endpoint(email_address)
         else
-          referee = Referee.new.set_endpoint(attributes[:email_address])
+          referee = Referee.new.set_endpoint(email_address)
         end
         referral = Referral.new.construct(referee.id)
         ThingMailer.send_referral(referral).deliver
