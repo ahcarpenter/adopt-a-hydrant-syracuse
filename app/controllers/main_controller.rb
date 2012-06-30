e649f81e6697181403462339fc2680bbe854a980
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-06-29 22:15:24 -0400

    update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,8 +2,7 @@ class MainController < ApplicationController
   before_filter :set_current_user
     
   def index
-    @app_name = t('titles.main')
-    ReferThis.url(params[:referral], User.current.id, request.base_url, User.current.name, @app_name) if !params[:referral].nil?
+    ReferThis.url(params[:referral], User.current.id, request.base_url, User.current.name, 'Adopt-a-Hydrant') if !params[:referral].nil?
     
     @required = Sidebar.translate_required if @required.nil?
     @sms_notifications = Sidebar.translate_sms_notifications if @sms_notifications.nil?
