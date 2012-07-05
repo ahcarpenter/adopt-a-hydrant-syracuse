be6403b7a18103734469a1cc321b4cf7ef5740f2
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-07-05 15:05:41 -0400

    update

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,18 +5,23 @@ class MainController < ApplicationController
     
   def index
     ReferThis.url(params[:referral], User.current.id, request.base_url, User.current.name, 'Adopt-a-Hydrant') if !params[:referral].nil?
-    @oauth = Koala::Facebook::OAuth.new(255900427854057, '8efe989aeb23f1206c40362da5795ba0', 'http://adopt-a-hydrant-syracuse.herokuapp.com/')
-    session[:url_for_oauth_code] = @oauth.url_for_oauth_code(:permissions=>'publish_stream', :callback_url=>'http://adopt-a-hydrant-syracuse.herokuapp.com/')
+    @oauth = Koala::Facebook::OAuth.new(257793937668716, 'f66a2340122a29bc70f440dabc708130', 'http://adopt-a-hydrant-syracuse.herokuapp.com/')
+    session[:url_for_oauth_code] = @oauth.url_for_oauth_code(:permissions=>'publish_stream', :permissions=>'email', :callback_url=>'http://adopt-a-hydrant-syracuse.herokuapp.com/')
     if !request[:code].nil?
       @graph = Koala::Facebook::API.new(@oauth.get_access_token(request[:code]))
       profile = @graph.get_object('me')
       if User.exists?(:facebook_id=>profile['id'])
+        user = User.find_by_facebook_id(profile['id'])
+        user.name = profile['name']
+        user.email = profile['email']
+        user.save
+        sign_in('user', user)
         User.current = User.find_by_facebook_id(profile['id'])
-        sign_in('user', User.current)
       else
         user = User.new1
         user.name = profile['name']
         user.facebook_id = profile['id']
+        user.email = profile['email']
         user.save
         sign_in('user', user)
         User.current = user
