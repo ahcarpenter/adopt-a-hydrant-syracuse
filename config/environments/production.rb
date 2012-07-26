3073191262848d260f7e6d9e711d8073d0c6b3ad
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-07-26 12:58:21 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,6 @@ AdoptAThing::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_controller.asset_host = 'https://adopt-a-hydrant-syracuse-assets.s3.amazonaws.com'
 end
 
 ActionMailer::Base.smtp_settings = {
