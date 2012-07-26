250fc52d6b387af3351221148f5a4ceb561aa6a8
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-07-26 08:33:53 -0400

    update

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,5 @@ module AdoptAThing
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     ActiveRecord::SessionStore::Session.primary_key = 'session_id'
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
