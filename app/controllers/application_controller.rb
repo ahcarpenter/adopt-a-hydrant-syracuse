c275d59698b09876500263f04cca56558bdd98d7
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-08-04 18:47:59 -0400

    Revert "Revert "tidy up""
    This reverts commit 1120afa291d6286a83908b204206ce87c1cece52.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ protected
       params[:flash].each do |key, message|
         if message.include? 'notices'
           I18n.locale = cookies[:locale]
-          flash.now[key.to_sym] = I18n.t(message)
+          flash.now[key.to_sym] = I18n.t message
         else
           flash.now[key.to_sym] = message
         end
@@ -34,29 +34,29 @@ protected
   end
 
   def set_locale
-    @@oauth = Koala::Facebook::OAuth.new(255900427854057, '8efe989aeb23f1206c40362da5795ba0', 'http://adopt-a-hydrant-syracuse.herokuapp.com/')
+    @@oauth = Koala::Facebook::OAuth.new 255900427854057, '8efe989aeb23f1206c40362da5795ba0', 'http://adopt-a-hydrant-syracuse.herokuapp.com/'
     if !request[:code].nil?
-      @@graph = Koala::Facebook::API.new(@@oauth.get_access_token(request[:code]))
-      @@profile = @@graph.get_object('me')
+      @@graph = Koala::Facebook::API.new @@oauth.get_access_token(request[:code])
+      @@profile = @@graph.get_object 'me'
     end
     if request.env['HTTP_USER_AGENT'].include? 'Mobi'
-      session[:url_for_oauth_code] = @@oauth.url_for_oauth_code(:permissions=>'publish_stream', :permissions=>'email', :callback_url=>'http://adopt-a-hydrant-syracuse.herokuapp.com/', :display=>'touch')
+      session[:url_for_oauth_code] = @@oauth.url_for_oauth_code :permissions=>'publish_stream', :permissions=>'email', :callback_url=>'http://adopt-a-hydrant-syracuse.herokuapp.com/', :display=>'touch'
     else
-      session[:url_for_oauth_code] = @@oauth.url_for_oauth_code(:permissions=>'publish_stream', :permissions=>'email', :callback_url=>'http://adopt-a-hydrant-syracuse.herokuapp.com/')
+      session[:url_for_oauth_code] = @@oauth.url_for_oauth_code :permissions=>'publish_stream', :permissions=>'email', :callback_url=>'http://adopt-a-hydrant-syracuse.herokuapp.com/'
     end
     
     available_languages = Dir.glob(Rails.root + 'config/locales/??.yml').map do |file|
-      File.basename(file, '.yml')
+      File.basename file, '.yml'
     end
     
     !params[:locale].nil? ? uri_slice = params[:locale].slice(0..1) : nil
-    (uri_slice == 'ar') || (uri_slice == 'cn') || (uri_slice == 'de') || (uri_slice == 'en') || (uri_slice == 'es') || (uri_slice == 'fr') || (uri_slice == 'gr') || (uri_slice == 'ht') || (uri_slice == 'he') || (uri_slice == 'it') || (uri_slice == 'kr') || (uri_slice == 'pl') || (uri_slice == 'pt') || (uri_slice == 'ru') || (uri_slice == 'yi') ? locale_token_in_uri = true : nil
+    uri_slice == 'ar' || uri_slice == 'cn' || uri_slice == 'de' || uri_slice == 'en' || uri_slice == 'es' || uri_slice == 'fr' || uri_slice == 'gr' || uri_slice == 'ht' || uri_slice == 'he' || uri_slice == 'it' || uri_slice == 'kr' || uri_slice == 'pl' || uri_slice == 'pt' || uri_slice == 'ru' || uri_slice == 'yi' ? locale_token_in_uri = true : nil
     
     !params[:locale].nil? && params[:locale].slice(2..params[:locale].length).length > 0 ? Referral.resolve_token(params[:locale].slice(2..params[:locale].length)) : nil
     
     locale_token_in_uri ? cookies[:locale] = uri_slice : nil
     
     I18n.locale = cookies[:locale] || I18n.default_locale
-    piece(:json, {:hydrants_adopted=>Thing.where('user_id is not null'), :hydrants_not_adopted=>Thing.where('user_id is null'), :hydrants_all=>Thing.all})
+    piece :json, {:hydrants_adopted=>Thing.where('user_id is not null'), :hydrants_not_adopted=>Thing.where('user_id is null'), :hydrants_all=>Thing.all}
   end
 end
