32591acf715622f6f8234b77efe80b698bb6ea5f
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-08-04 18:47:59 -0400

    Revert "Revert "tidy up""
    This reverts commit 7e336adfa091cf1a635bece91f245f45099337b7.

--- app/controllers/info_window_controller.rb
+++ app/controllers/info_window_controller.rb
@@ -1,24 +1,24 @@
 # http://oldwiki.rubyonrails.org/rails/pages/HowtoWorkWithSessions
 class InfoWindowController < ApplicationController
   def index
-    session[:thing] = Thing.find_by_id(params[:thing_id])
-    session[:thing] = Thing.find_by_id(session[:id]) if session[:thing].nil?
+    session[:thing] = Thing.find_by_id params[:thing_id]
+    session[:thing] = Thing.find_by_id session[:id] if session[:thing].nil?
     
     if session[:thing].adopted?
       if user_signed_in? && current_user.id == session[:thing].user_id
         if session[:conflict] == true
-          render('users/conflict')
+          render 'users/conflict'
         else
-          render('users/thank_you')
+          render 'users/thank_you'
         end
       else
-        render('users/profile')
+        render 'users/profile'
       end
     else
       if user_signed_in?
-        render('things/adopt')
+        render 'things/adopt'
       else
-        render('users/sign_in')
+        render 'users/sign_in'
       end
     end
   end
