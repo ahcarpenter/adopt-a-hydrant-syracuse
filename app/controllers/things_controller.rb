1559168d37c9837616852c367d169285cd7c73f8
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-08-05 17:52:58 -0400

    update

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -15,7 +15,7 @@ class ThingsController < ApplicationController
   def update
     session[:conflict] = false
     session[:id] = session[:thing].id
-    session[:thing].update_attributes(params[:thing]) ? respond_with session[:thing] : render(:json => {'errors' => session[:thing].errors}, :status => 500)
+    session[:thing].update_attributes(params[:thing]) ? respond_with(session[:thing]) : render(:json => {'errors' => session[:thing].errors}, :status => 500)
     rescue ActiveRecord::StaleObjectError
       session[:conflict] = true
       redirect_to :controller => 'info_window', :action => 'index'
