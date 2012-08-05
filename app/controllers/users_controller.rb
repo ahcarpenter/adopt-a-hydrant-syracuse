25a190b117a1b4e48642454f640877696ca579f2
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-08-05 16:58:24 -0400

    tidy up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < Devise::RegistrationsController
   def edit
-    render('sidebar/edit_profile', :layout => 'sidebar')
+    render 'sidebar/edit_profile', :layout => 'sidebar'
   end
   
   def update
     if resource.update_without_password(params[resource_name])
-      sign_in(resource_name, resource, :bypass => true)
+      sign_in resource_name, resource, :bypass => true
       flash[:notice] = I18n.t('notices.profile_updated')
-      redirect_to(:controller => 'sidebar', :action => 'search')
+      redirect_to :controller => 'sidebar', :action => 'search'
     else
-      clean_up_passwords(resource)
-      render(:json => {'errors' => resource.errors}, :status => 500)
+      clean_up_passwords resource
+      render :json => {'errors' => resource.errors}, :status => 500
     end
   end
 
@@ -18,14 +18,14 @@ class UsersController < Devise::RegistrationsController
     build_resource
     if resource.save
       begin
-      sign_in(resource_name, resource)
+      sign_in resource_name, resource
       rescue ActiveRecord::RecordNotUnique
         flash[:notice] = 'An account with that email address already exists!'
       end
-      render(:json => resource)
+      render :json => resource
     else
-      clean_up_passwords(resource)
-      render(:json => {'errors' => resource.errors}, :status => 500)
+      clean_up_passwords resource
+      render :json => {'errors' => resource.errors}, :status => 500
     end
   end
 end
